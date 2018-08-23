d2f579123cb30b32b69f2aa59c45aef483d4bb49
shanart <shanin.x@gmail.com>
2018-08-23 17:42:05 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+func ExampleScrape() {
+	// Request the HTML page.
+	res, err := http.Get("https://world-of-contract.com/team/franz-josef-bartsch")
+    var images []string
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find("figure.profil-bild").Each(func(i int, s *goquery.Selection) {
+
+		src, ok := s.Find("img").Attr("src")
+        if ok {
+            images = append(images, src)
+            fmt.Println(src)
+        }
+
+	})
+
+}
+
+func main() {
+	ExampleScrape()
+}
