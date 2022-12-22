521e97edc7a4dda2f21cdde3c02b17479b6e23b7
Alex <shanin.x@gmail.com>
2022-12-22 09:56:58 +0200

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -5,9 +5,18 @@ def download_videos(url):
 #     api = Api(api_key='<youtube api key>')
 #     video = api.get_video_by_id(video_id=url.replace('https://www.youtube.com/watch?v=', ''), return_json=True)
     yt_obj = YouTube(url)
-    filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
-    filters.get_highest_resolution().download()
+    if only_audio:
+        filters = yt_obj.streams.filter(only_audio=True)
+        filters.get_audio_only().download()
+    else:
+        filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
+        filters.get_highest_resolution().download()
   
 if __name__ == "__main__":
     url = input("Please enter the video link: ")
-    download_videos(url)
+    audio = input("Download only audio? (y/n): ")
+    if audio == 'y':
+        download_videos(url, only_audio=True)
+    else:
+        download_videos(url)
+
